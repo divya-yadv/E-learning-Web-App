d49bd362d32fa7a2d46d4f24ef0b82ac37f643de
divya <divyayadavg8@gmail.com>
2022-05-31 19:50:11 +0530

    build the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import Data from './data';
 
 function App() {
   return (
@@ -6,7 +6,25 @@ function App() {
       <header>
         <a href="/">SmartLearn</a>
       </header>
-      <main></main>
+      <main>
+        <h1>Featured Courses</h1>
+        <div className="courses">
+          {Data.courses.map((course) => {
+            return (
+              <div className="course" key={course.id}>
+                <img src={course.image} alt={course.Course_name} />
+                <div className="course-info">
+                  <p>{course.Course_name}</p>
+                  <p>{course.course_instructor}</p>
+                  <p>
+                    <strong>${course.price}</strong>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
     </div>
   );
 }

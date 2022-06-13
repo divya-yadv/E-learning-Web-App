d76b2778782493840fff17be13ec59b71795a9f5
divya <divyayadavg8@gmail.com>
2022-06-13 14:45:08 +0530

    update

--- frontend/src/components/UpdateProfile.js
+++ frontend/src/components/UpdateProfile.js
@@ -58,20 +58,20 @@ export default function UpdateProfile() {
     try {
       setLoading(false);
       setError('');
-      // try {
-      //   const res = await axios.post('/api/users/updateuser', {
-      //     email: currentUser.email,
-      //     name: name,
-      //     user_name: username,
-      //   });
-      //   console.log(res);
-      // } catch (error) {
-      //   setError(error);
-      //   setLoading(false);
-      // }
-      if (password) {
-        await updatepassword(password);
+      try {
+        const res = await axios.post('/api/users/updateuser', {
+          email: currentUser.email,
+          name: name,
+          user_name: username,
+        });
+        console.log(res);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
       }
+      // if (password) {
+      //   await updatepassword(password);
+      // }
       navigate('/dashboard');
     } catch (error) {
       getError(error);

ead1f1a8503d82bc959c425007527a53457d9938
divya <divyayadavg8@gmail.com>
2022-06-18 19:50:25 +0530

    remove get course file

--- frontend/src/screens/TeacherDashboard.js
+++ frontend/src/screens/TeacherDashboard.js
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import GetCourse from '../components/GetCourse';
+import Rating from '../components/Rating';
 import { Store } from '../store';
 
 function TeacherDashboard() {
@@ -17,10 +18,44 @@ function TeacherDashboard() {
 
       <Row className="mt-5">
         {userInfo.createdCourses &&
-          userInfo.createdCourses.map((courseid, index) => (
+          userInfo.createdCourses.map((course, index) => (
             <Col key={index} sm={6} md={4} lg={4} className="mb-3">
-              {console.log(courseid)};
-              <GetCourse courseid={courseid._id} />
+              {console.log(course)};
+              <Card className="shadow ">
+                <Link to={`/courses/yours/slug/${course.slug}`}>
+                  <Card.Img
+                    className="card-img-top imagewidth"
+                    src={course.thumbnail}
+                    alt={course.Course_name}
+                    height={250}
+                    width={250}
+                  />
+                </Link>
+                <Card.Body>
+                  <Link
+                    className="title"
+                    to={`/courses/yours/slug/${course.slug}`}
+                  >
+                    <Card.Title>{course.Course_name}</Card.Title>
+                  </Link>
+                  <Card.Text>{course.description}</Card.Text>
+                  <Rating
+                    rating={course.rating}
+                    numReviews={course.numReviews}
+                  />
+                  <Card.Text>
+                    <strong>${course.price}</strong>
+                  </Card.Text>
+                </Card.Body>
+                <Link
+                  className="text-decoration-none text-white"
+                  to={`/courses/update/slug/${course.slug}`}
+                >
+                  <Button className="btn btn-primary w-100">
+                    Update Course
+                  </Button>
+                </Link>
+              </Card>
             </Col>
           ))}
       </Row>
